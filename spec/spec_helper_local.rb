b452cf7c4bf60f36dc7aa0ba9021fc6d38f179d5
Mike Fröhner <m.froehner@syseleven.de>
2023-03-23 09:07:10 +0100

    adjust rspec includes

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+require 'shared_examples/config'
+require 'shared_examples/install_archive'
+require 'shared_examples/install_package'
+require 'shared_examples/install_repository_redhat'
+require 'shared_examples/install_repository'
+require 'shared_examples/install'
+require 'shared_examples/service'
+
+require 'helper/get_defaults'
+
 TESTS = {
   'with default value' => {},
   'with installation via archive' => {
